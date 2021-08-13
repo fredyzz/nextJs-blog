ae1b0b6ce928eed6601e9816ae912f4a0d5f8648
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2021-08-13 14:13:15 +0200

    Create Footer  component

---
+++ components/Footer/index.js
@@ -0,0 +1,15 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => (
+  <footer className={styles.Footer}>
+    <a
+      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Powered by Vercel
+    </a>
+  </footer>
+);
+
+export default Footer;
